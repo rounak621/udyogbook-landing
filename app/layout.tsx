794eb3ebc13b1ba262860ea584bc42c356bbb8ad
Rounak Sushil Choudhary <rounak@Rounaks-MacBook-Air.local>
2026-04-12 18:04:31 +0530

    Add Google Analytics GA4 and Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en-IN">
       <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKM2HJ66');`
+        }} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CTGVDRNMNE" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CTGVDRNMNE');
+          `
+        }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -102,6 +118,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKM2HJ66"
+            height="0"
+            width="0"
+            style={{display:'none', visibility:'hidden'}}
+          />
+        </noscript>
         <style suppressHydrationWarning>{`
           *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
           html { scroll-behavior: smooth; }
